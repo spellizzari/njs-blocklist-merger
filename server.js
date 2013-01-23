c2ef2cc4d76baee6a8fd92e48d3c6dc90b239d7d
eva03 <sebastien.pellizzari@gmail.com>
2013-01-23 08:01:48 +0100

    Added the first version of server.ts and compiled server.js

---
+++ server.js
@@ -0,0 +1,228 @@
+var async = require("async");
+
+var http = require("http")
+var zlib = require("zlib")
+var url = require("url")
+var fs = require("fs")
+var AsyncHTTPRequestTask = (function () {
+    function AsyncHTTPRequestTask(requestUrl) {
+        this._total = 0;
+        this._error = null;
+        this._result = null;
+        this._progress = 0;
+        this._isSuccess = false;
+        this._requestUrl = requestUrl;
+    }
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "Error", {
+        get: function () {
+            return this._error;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "Total", {
+        get: function () {
+            return this._total;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "Result", {
+        get: function () {
+            return this._result;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "Progress", {
+        get: function () {
+            return this._progress;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "IsSuccess", {
+        get: function () {
+            return this._isSuccess;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AsyncHTTPRequestTask.prototype, "RequestUrl", {
+        get: function () {
+            return this._requestUrl;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AsyncHTTPRequestTask.prototype.GetAsyncTask = function () {
+        var This = this;
+        return function (cllbck) {
+            This.InternalExecute(cllbck);
+        }
+    };
+    AsyncHTTPRequestTask.prototype.InternalExecute = function (callback) {
+        var This = this;
+        AsyncHTTPRequestTask.InternalHttpGet(this._requestUrl, function (response) {
+            var output = null;
+            switch(response.headers['content-encoding']) {
+                case 'gzip': {
+                    var gzip = zlib.createGunzip();
+                    response.pipe(gzip);
+                    output = gzip;
+                    break;
+
+                }
+                case 'deflate': {
+                    var inflate = zlib.createInflate();
+                    response.pipe(inflate);
+                    output = inflate;
+                    break;
+
+                }
+                default: {
+                    output = response;
+                    break;
+
+                }
+            }
+            This._result = "";
+            This._total = parseInt(response.headers["content-length"]);
+            This._progress = 0;
+            output.on("data", function (chunk) {
+                This._progress += chunk.length;
+                This._result += chunk.toString("utf-8");
+            });
+            output.on("end", function () {
+                This._isSuccess = true;
+                callback(null, This);
+            });
+            output.on("close", function () {
+                if(!This._isSuccess) {
+                    This._error = new Error("the underlying connection was terminated before all data was sent");
+                    callback(null, This);
+                }
+            });
+        }, function (error) {
+            This._isSuccess = false;
+            This._error = error;
+            callback(null, This);
+        });
+    };
+    AsyncHTTPRequestTask.InternalHttpGet = function InternalHttpGet(requestUrl, success, error) {
+        var parsedRequestUrl = url.parse(requestUrl);
+        var options = parsedRequestUrl;
+        options.headers = {
+            'Accept-Cncoding': 'gzip,deflate'
+        };
+        var request = http.get(options, function (response) {
+            if(response.statusCode > 300 && response.statusCode < 400 && response.headers.location) {
+                var parsedRedirectUrl = url.parse(response.headers.location);
+                if(!parsedRedirectUrl.hostname) {
+                    parsedRedirectUrl.hostname = parsedRequestUrl.hostname;
+                }
+                AsyncHTTPRequestTask.InternalHttpGet(url.format(parsedRedirectUrl), success, error);
+            } else {
+                if(response.statusCode == 200) {
+                    success(response);
+                } else {
+                    error(new Error("HTTP request failed with " + response.statusCode + " response code"));
+                }
+            }
+        });
+        request.on("error", error);
+    }
+    return AsyncHTTPRequestTask;
+})();
+var MergedBlocklistServer = (function () {
+    function MergedBlocklistServer() { }
+    MergedBlocklistServer.prototype.Listen = function (hostname, port) {
+        var This = this;
+        var server = http.createServer(function (request, response) {
+            This.ProcessRequest(request, response);
+        });
+        server.listen(port, hostname);
+    };
+    MergedBlocklistServer.prototype.ProcessRequest = function (request, response) {
+        console.info("Got HTTP request for %s", request.url);
+        var requestUrl = url.parse(request.url);
+        if(requestUrl.pathname && !requestUrl.query) {
+            var listFilePath = "./lists" + requestUrl.pathname;
+            if(!fs.existsSync(listFilePath)) {
+                response.writeHead(404, {
+                    "Content-Type": "text/plain"
+                });
+                response.end("404 Not Found\nSpecified file could not be found\n");
+                return;
+            }
+            var listText = fs.readFileSync(listFilePath, "utf-8");
+            if(listText.charAt(0) === '\uFEFF') {
+                listText = listText.substr(1);
+            }
+            var listUrls = listText.split("\n").map(function (value) {
+                return value.trim();
+            }).filter(function (value) {
+                return !value.match(/^#.*$/) && !value.match(/^\w*$/);
+            });
+            response.writeHead(200, {
+                "Content-Type": "text/plain",
+                "Content-Encoding": "utf-8"
+            });
+            MergedBlocklistServer.MergeListsAsync(listUrls, function (mergedData, failedUrls) {
+                for(var i = 0; i < failedUrls.length; i++) {
+                    console.error("ERROR: could not fetch data from %s", failedUrls[i]);
+                }
+                response.end(mergedData, "utf-8");
+            });
+        } else {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.end("404 Not Found\n");
+        }
+    };
+    MergedBlocklistServer.MergeListsAsync = function MergeListsAsync(listUrls, completionCallback) {
+        var tasks = [];
+        for(var i = 0; i < listUrls.length; i++) {
+            tasks.push(new AsyncHTTPRequestTask(listUrls[i] + "&fileformat=p2p&archiveformat=gz").GetAsyncTask());
+        }
+        async.parallel(tasks, function (error, results) {
+            var failedUrls = [];
+            var mergedResult = "";
+            for(var i = 0; i < results.length; i++) {
+                if(!results[i].IsSuccess) {
+                    failedUrls.push(results[i].RequestUrl);
+                    mergedResult += "# failed to load url: " + results[i].RequestUrl + "\n";
+                }
+            }
+            for(var i = 0; i < results.length; i++) {
+                if(results[i].IsSuccess) {
+                    mergedResult += "# list from " + results[i].RequestUrl + "\n";
+                    mergedResult += results[i].Result + "\n";
+                }
+            }
+            completionCallback(mergedResult, failedUrls);
+        });
+    }
+    return MergedBlocklistServer;
+})();
+var arguments = new Object();
+var argumentRegex = /^([a-zA-Z_][a-zA-Z_0-9]*)=('?[^']*'?)/;
+for(var i = 2; i < process.argv.length; i++) {
+    var match = argumentRegex.exec(process.argv[i]);
+    if(match) {
+        arguments[match[1]] = match[2];
+    }
+}
+var server = new MergedBlocklistServer();
+var port = 1337;
+var hostname = "127.0.0.1";
+if(arguments.host) {
+    hostname = arguments.host;
+}
+if(arguments.port) {
+    port = parseInt(arguments.port);
+}
+server.Listen(hostname, port);
+console.log("Server is listening on %s:%d...", hostname, port);
+//@ sourceMappingURL=server.js.map
---
+++ server.ts
@@ -0,0 +1,333 @@
+/// <reference path="typings/node/node-0.8.d.ts" />
+/// <reference path="typings/async/async-0.1.d.ts" />
+
+// Import nodejs external modules.
+var async = <Async>require("async");
+
+// Import core modules.
+import stream = module("stream");
+import http = module("http");
+import zlib = module("zlib");
+import url = module("url");
+import fs = module("fs");
+
+// Represents an asynchronous HTTP request.
+class AsyncHTTPRequestTask
+{
+    private _error: Error;                      // Error object, if any.
+    private _total: number;                     // Total number of bytes to receive.
+    private _result: string;                    // Result data, if any.
+    private _progress: number;                  // Current progress.
+    private _isSuccess: bool;                   // Has the task completed successfully?
+    private _requestUrl: string;                // The request url.
+
+    //=======================================================================
+    //     Properties
+    //=======================================================================
+
+    // Gets the error object, if any.
+    public get Error(): Error { return this._error; }
+    // Gets the total number of bytes to receive.
+    public get Total(): number { return this._total; }
+    // Gets the HTTP result data, if any.
+    public get Result(): string { return this._result; }
+    // Gets the current number of bytes received.
+    public get Progress(): number { return this._progress; }
+    // Gets whether the task has completed successfully.
+    public get IsSuccess(): bool { return this._isSuccess; }
+    // Gets the request url string.
+    public get RequestUrl(): string { return this._requestUrl; }
+
+    //=======================================================================
+    //     Constructors
+    //=======================================================================
+
+    // Constructor.
+    constructor (requestUrl: string)
+    {
+        this._total = 0;
+        this._error = null;
+        this._result = null;
+        this._progress = 0;
+        this._isSuccess = false;
+        this._requestUrl = requestUrl;
+    }
+
+    //=======================================================================
+    //     Methods
+    //=======================================================================
+
+    // Returns a task to pass async.* functions.
+    public GetAsyncTask()
+    {
+        // Anonymous function closure for callback.
+        var This = this;
+        return (cllbck: AsyncCallback) =>
+        {
+            This.InternalExecute(cllbck);
+        };
+    }
+
+    //=======================================================================
+    //     Non Public
+    //=======================================================================
+
+    // Executes the task asynchronously.
+    private InternalExecute(callback: AsyncCallback)
+    {
+        // For closure of callbacks.
+        var This = this;
+
+        // Do the HTTP get.
+        AsyncHTTPRequestTask.InternalHttpGet(this._requestUrl,
+            (response: http.ClientResponse) =>
+            {
+                // Unzip content if necessary.
+                var output = <stream.ReadableStream>null;
+                switch (response.headers['content-encoding'])
+                {
+                    case 'gzip':
+                        var gzip = zlib.createGunzip();
+                        response.pipe(gzip);
+                        output = gzip;
+                        break;
+                    case 'deflate':
+                        var inflate = zlib.createInflate();
+                        response.pipe(inflate);
+                        output = inflate;
+                        break;
+                    default:
+                        output = response;
+                        break;
+                }
+
+                // Prepare for download.
+                This._result = "";
+                This._total = parseInt(response.headers["content-length"]);
+                This._progress = 0;
+
+                // Register "data" event.
+                output.on("data", function (chunk: any)
+                {
+                    // Add data that was read.
+                    This._progress += chunk.length;
+                    This._result += chunk.toString("utf-8");
+                });
+
+                // Register "end" event.
+                output.on("end", function ()
+                {
+                    // We have successfully downloaded all the content.
+                    This._isSuccess = true;
+                    callback(null, This);
+                });
+
+                // Register "close" event.
+                output.on("close", function ()
+                {
+                    // Error if it occurs before "end" event.
+                    if (!This._isSuccess)
+                    {
+                        // Notify.
+                        This._error = new Error("the underlying connection was terminated before all data was sent");
+                        callback(null, This);
+                    }
+                });
+            },
+            (error: Error) =>
+            {
+                // Notify error.
+                This._isSuccess = false;
+                This._error = error;
+                callback(null, This);
+            });
+    }
+
+    // Does a direct HTTP GET at specified url by following any redirection (warning: redirection loops will cause a stack overflow).
+    private static InternalHttpGet(requestUrl: string, success: (response: http.ClientResponse) => void , error: (error: Error) => void )
+    {
+        // Parse the request url.
+        var parsedRequestUrl = url.parse(requestUrl);
+
+        // Build HTTP request options by accepting encodings that blocklist providers use.
+        var options = <any>parsedRequestUrl;
+        options.headers =
+        {
+            'Accept-Cncoding': 'gzip,deflate'
+        };
+
+        // Do the HTTP GET.
+        var request = http.get(options, function (response: http.ClientResponse) =>
+        {
+            // If it's a redirection...
+            if (response.statusCode > 300 && response.statusCode < 400 && response.headers.location)
+            {
+                // Parse redirection url.
+                var parsedRedirectUrl = url.parse(response.headers.location);
+
+                // Set hostname if it's a relative redirection.
+                if (!parsedRedirectUrl.hostname)
+                    parsedRedirectUrl.hostname = parsedRequestUrl.hostname;
+
+                // Follow redirection.
+                InternalHttpGet(url.format(parsedRedirectUrl), success, error);
+            }
+                // If it's a 200 OK...
+            else if (response.statusCode == 200)
+            {
+                // Call success callback.
+                success(response);
+            }
+            else
+            {
+                // Call error callback.
+                error(new Error("HTTP request failed with " + response.statusCode + " response code"));
+            }
+        });
+
+        // Register "error" event.
+        request.on("error", error);
+    }
+}
+
+// Represents a class for an HTTP server that merges a set of blocklists on request.
+class MergedBlocklistServer
+{
+    //=======================================================================
+    //     Methods
+    //=======================================================================
+
+    // Starts the server on specified port.
+    public Listen(hostname: string, port: number)
+    {
+        var This = this;
+
+        // Create HTTP server.
+        var server = http.createServer((request: http.ServerRequest, response: http.ServerResponse) =>
+        {
+            // Process the request.
+            This.ProcessRequest(request, response);
+        });
+
+        // Listen.
+        server.listen(port, hostname);
+    }
+
+    //=======================================================================
+    //     Non Public
+    //=======================================================================
+
+    // Processes specified request.
+    private ProcessRequest(request: http.ServerRequest, response: http.ServerResponse)
+    {
+        // Log it.
+        console.info("Got HTTP request for %s", request.url);
+
+        // Parse request url.
+        var requestUrl = url.parse(request.url);
+        if (requestUrl.pathname && !requestUrl.query)
+        {
+            // Build local file path.
+            var listFilePath = "./lists" + requestUrl.pathname;
+
+            // Make sure it exists.
+            if (!fs.existsSync(listFilePath))
+            {
+                // Respond with 404.
+                response.writeHead(404,
+                {
+                    "Content-Type": "text/plain"
+                });
+                response.end("404 Not Found\nSpecified file could not be found\n");
+                return;
+            }
+
+            // Read file content.
+            var listText = fs.readFileSync(listFilePath, "utf-8");
+            if (listText.charAt(0) === '\uFEFF') listText = listText.substr(1);
+
+            // Split into lines, ignore empty lines and lines starting with #.
+            var listUrls = listText.split("\n").map((value: string) => value.trim()).filter((value: string) => !value.match(/^#.*$/) && !value.match(/^\w*$/));
+
+            // Write OK.
+            response.writeHead(200, {
+                "Content-Type": "text/plain",
+                "Content-Encoding": "utf-8"
+            });
+
+            // Merge them asynchronously.
+            MergedBlocklistServer.MergeListsAsync(listUrls, (mergedData: string, failedUrls: string[]) =>
+            {
+                // Log failures.
+                for (var i = 0; i < failedUrls.length; i++)
+                    console.error("ERROR: could not fetch data from %s", failedUrls[i]);
+
+                // Write result.
+                response.end(mergedData, "utf-8");
+            });
+        }
+        else
+        {
+            // Respond with 404.
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.end("404 Not Found\n");
+        }
+    }
+
+    // Merges the lists downloaded from specified urls asynchronously.
+    private static MergeListsAsync(listUrls: string[], completionCallback: (mergedData: string, failedUrls: string[]) => void )
+    {
+        var tasks = [];
+
+        // Build the list of tasks.
+        for (var i = 0; i < listUrls.length; i++)
+            tasks.push(new AsyncHTTPRequestTask(listUrls[i] + "&fileformat=p2p&archiveformat=gz").GetAsyncTask());
+
+        // Call asynchronously.
+        async.parallel(tasks, (error: string, results: AsyncHTTPRequestTask[]) =>
+        {
+            var failedUrls = <string[]>[];
+            var mergedResult = "";
+
+            // Write failures as comments at the beginning of the file.
+            for (var i = 0; i < results.length; i++)
+                if (!results[i].IsSuccess)
+                {
+                    failedUrls.push(results[i].RequestUrl);
+                    mergedResult += "# failed to load url: " + results[i].RequestUrl + "\n";
+                }
+
+            // Write data.
+            for (var i = 0; i < results.length; i++)
+                if (results[i].IsSuccess)
+                {
+                    mergedResult += "# list from " + results[i].RequestUrl + "\n";
+                    mergedResult += results[i].Result + "\n";
+                }
+
+            // Call callback.
+            completionCallback(mergedResult, failedUrls);
+        });
+    }
+}
+
+// Parse arguments.
+var arguments = <any>new Object();
+var argumentRegex = <RegExp>/^([a-zA-Z_][a-zA-Z_0-9]*)=('?[^']*'?)/;
+for (var i = 2; i < process.argv.length; i++)
+{
+    var match = argumentRegex.exec(process.argv[i]);
+    if (match)
+        arguments[match[1]] = match[2];
+}
+
+// Create the web server.
+var server = new MergedBlocklistServer();
+var port = 1337;
+var hostname = "127.0.0.1";
+if (arguments.host) hostname = arguments.host;
+if (arguments.port) port = parseInt(arguments.port);
+server.Listen(hostname, port);
+console.log("Server is listening on %s:%d...", hostname, port);
